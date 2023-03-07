5df556e907aafadac1d9106fadde1b617268132d
CacheTtwo <manuelalejandrotorres2905@outlook.com>
2023-03-07 17:51:10 +0100

    Version actualizada modificada 07/03/2023

--- src/PaqC01/DiseñoContenedores.java
+++ src/PaqC01/DiseñoContenedores.java
@@ -1,8 +1,11 @@
 package PaqC01;
 
 import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DiseñoContenedores extends JFrame {
     private JLabel NumId;
@@ -36,14 +39,7 @@ public class DiseñoContenedores extends JFrame {
     private JLabel Estado;
     private JTextArea Estad_text;
     private JLabel Logo;
-
-    private String var1;
-    private String var2;
-    private String var3;
-
-    public String getVar1() {
-        return var1;
-    }
+    private JLabel Mensajes;
 
     public DiseñoContenedores() {
         setContentPane(mainPanel);
@@ -51,29 +47,158 @@ public class DiseñoContenedores extends JFrame {
         setSize(1500, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        NumIdtext.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Introduzca el número de identificación.");
+                try {
+                    int dato =Integer.parseInt(NumIdtext.getText());
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "El dato no es un entero, vuelve a teclearlo.");
+                }
+            }
+        });
+        Pesotext.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Introduzca el peso en toneladas.");
+                try {
+                    int dato = Integer.parseInt(Pesotext.getText());
+                } catch(NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null,"El dato no es un entero, vuelve a teclearlo.");
+                }
+            }
+        });
+        Desctext.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Complete la descripción del contenido.");
+                //En este apartado no imponemos restricción
+            }
+        });
+        Emp_remtext.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Escriba el nombre de la empresa remitente.");
+                try {
+                    Integer.toString(Integer.parseInt(Emp_remtext.getText()));
+                    JOptionPane.showMessageDialog(null,"El dato no es correcto (debe ser String), vuelve a teclearlo.");
+                } catch(NumberFormatException ex) {
+                }
+            }
+        });
+        Emp_rectext.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Escriba el nombre de la empresa receptora.");
+                try {
+                    Integer.toString(Integer.parseInt(Emp_rectext.getText()));
+                    JOptionPane.showMessageDialog(null,"El dato no es correcto (debe ser String), vuelve a teclearlo.");
+                } catch(NumberFormatException ex) {
+                }
+            }
+        });
+        Ap_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Contenedor apilado.");
+            }
+        });
+        Desap_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Contenedor desapilado.");
+            }
+        });
+        numCol_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Introduzca el número de columna de la que hay que desapilar");
+                try {
+                    int dato = Integer.parseInt(numCol_text.getText());
+                } catch(NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null,"El dato no es un entero, vuelve a teclearlo.");
+                }
+            }
+        });
         ContData_button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Mostrando datos del contenedor.");
                 DiseñoContenedoresP2 newframe = new DiseñoContenedoresP2();
-
                 newframe.setVisible(true);
-
-
             }
         });
-            JTextField NumIdText= new JTextField(10);
-            NumIdText.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e)
-                {
-                    DiseñoContenedoresP2 aux= new DiseñoContenedoresP2(NumIdText);
-
+        ID_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Introduzca el ID del contenedor");
+                try {
+                    int dato = Integer.parseInt(ID_text.getText());
+                } catch(NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null,"El dato no es válido para la ID.");
                 }
-            });
-
-
-
-
+            }
+        });
+        Cuantos_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Cantidad registrada.");
+            }
+        });
+        cuantosPais_box.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Ciudad.");
+            }
+        });
+        Cant_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Introduzca la cantidad");
+                try {
+                    int dato = Integer.parseInt(Cant_text.getText());
+                } catch(NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null,"El dato no es un entero, vuelve a teclearlo.");
+                }
+            }
+        });
+        Pais_procbox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("País de procedencia registrado.");
+            }
+        });
+        a1RadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Prioridad anotada (Marque solo una).");
+            }
+        });
+        a2RadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Prioridad anotada (Marque solo una).");
+            }
+        });
+        a3RadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Prioridad anotada (Marque solo una).");
+            }
+        });
+        Insp_Aduanas.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes.setText("Inspección confirmada.");
+            }
+        });
+        Estad_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes.setText("Plano del hub.");
+            }
+        });
     }
 
     public DiseñoContenedores(JLabel logo) throws HeadlessException {
@@ -83,6 +208,5 @@ public class DiseñoContenedores extends JFrame {
 
     public static void main(String[] args) {
         DiseñoContenedores cont = new DiseñoContenedores();
-
     }
 }
--- src/PaqC01/DiseñoContenedoresP2.java
+++ src/PaqC01/DiseñoContenedoresP2.java
@@ -1,7 +1,11 @@
 package PaqC01;
 
 import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DiseñoContenedoresP2 extends  JFrame {
     private JLabel NumID;
@@ -9,11 +13,11 @@ public class DiseñoContenedoresP2 extends  JFrame {
     private JLabel Descripcion;
     private JLabel EmpRem;
     private JLabel EmpRec;
-    private JTextField NumID_text;
-    private JTextField Peso_text;
-    private JTextArea Descripcion_text;
-    private JTextField EmpRem_text;
-    private JTextField EmpRec_text;
+    public JTextField NumID_text;
+    public JTextField Peso_text;
+    public JTextArea Descripcion_text;
+    public JTextField EmpRem_text;
+    public JTextField EmpRec_text;
     private JLabel Pais;
     private JTextField Pais_text;
     private JLabel Prioridad;
@@ -22,21 +26,74 @@ public class DiseñoContenedoresP2 extends  JFrame {
     private JRadioButton a3RadioButton;
     private JCheckBox Inspeccionado;
     private JPanel mainPanel;
-
-    public DiseñoContenedoresP2(JTextField numID_text) throws HeadlessException {
-        NumID_text = numID_text;
-    }
+    private JLabel Mensajes2;
 
     public DiseñoContenedoresP2() {
-        DiseñoContenedores aux= new DiseñoContenedores();
         setContentPane(mainPanel);
         setTitle("Welcome");
         setSize(800, 300);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
         setVisible(true);
-
-
-
+        NumID_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes2.setText("Número de identificación de la empresa.");
+            }
+        });
+        Peso_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes2.setText("Peso del contenedor.");
+            }
+        });
+        Descripcion_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes2.setText("Descripción del contenido del contenedor.");
+            }
+        });
+        EmpRem_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes2.setText("Empresaque envía el contenedor.");
+            }
+        });
+        EmpRec_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes2.setText("Empresa que recibe el contenedor.");
+            }
+        });
+        Pais_text.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                Mensajes2.setText("País del que proviene el contenedor.");
+            }
+        });
+        a1RadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes2.setText("Prioridad del contenedor.");
+            }
+        });
+        a2RadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes2.setText("Prioridad del contenedor.");
+            }
+        });
+        a3RadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes2.setText("Prioridad del contenedor.");
+            }
+        });
+        Inspeccionado.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Mensajes2.setText("Inspeccionado en aduanas (Sí/No).");
+            }
+        });
     }
 
     public static void main(String[] args) {
